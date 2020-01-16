c43e9a946cbbf2bb4e351969ff2ad722f23f68b2
FABIANA GEMMA GIANNUZZI <giannuzzifabianagemma@gmail.com>
2020-01-16 17:29:54 +0100

    Recode political position drop missing

--- script/import_data.R
+++ script/import_data.R
@@ -16,6 +16,7 @@ view_election01 <- filter(
 view(view_election01)
 
 
+#Recode variable political position
 view_election02 <- mutate(
   view_election01, 
   political_pos = ifelse(view_election01$left_right <= 3.3333, "left",
@@ -24,11 +25,12 @@ view_election02 <- mutate(
 )
 view(view_election02)
 
-
+#drop missing values 
 view_election03 <- na.omit(view_election02)
 dim(view_election03)
 
 apply(view_election03, 2, function(x) sum(is.na(x)))
 
-
 view(view_election03)
+
+
