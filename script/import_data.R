0751245d73e2d85f903b92fc5dc3d096229ad2e4
Anna Ludovica Vinelli <annaludovica.vinelli@studenti.unimi.it>
2020-01-16 17:27:42 +0100

    Recoding variables

--- script/import_data.R
+++ script/import_data.R
@@ -15,3 +15,20 @@ view_election01 <- filter(
   country_name %in% c("Italy", "Germany", "France", "Canada", "Australia", "Japan")) 
 view(view_election01)
 
+
+view_election02 <- mutate(
+  view_election01, 
+  political_pos = ifelse(view_election01$left_right <= 3.3333, "left",
+                   ifelse(view_election01$left_right <= 6.6666, "center",
+                          "right"))
+)
+view(view_election02)
+
+
+view_election03 <- na.omit(view_election02)
+dim(view_election03)
+
+apply(view_election03, 2, function(x) sum(is.na(x)))
+
+
+view(view_election03)
