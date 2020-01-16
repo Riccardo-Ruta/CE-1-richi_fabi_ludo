d7533a00a58af25b276302c24e8928e4d82d3ab4
FABIANA GEMMA GIANNUZZI <giannuzzifabianagemma@gmail.com>
2020-01-16 17:07:56 +0100

    Selected variables

--- script/import_data.R
+++ script/import_data.R
@@ -3,4 +3,5 @@ view_election <- read_csv("data/original_data/view_election.csv")
 View(view_election)
 
 library(tidyverse)
-select (view_election, country_name, election_date, vote_share, left_right)
+view_election00 <- select(view_election, country_name, election_date, vote_share, left_right)
+view(view_election00)
