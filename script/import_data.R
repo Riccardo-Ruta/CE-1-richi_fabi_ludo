199454c49ab6f34a624d083f8f1ba53f5217a5c7
FABIANA GEMMA GIANNUZZI <giannuzzifabianagemma@gmail.com>
2020-01-16 17:15:17 +0100

    Filter observation country

--- script/import_data.R
+++ script/import_data.R
@@ -1,7 +1,17 @@
+#Import data
 library(readr)
 view_election <- read_csv("data/original_data/view_election.csv")
 View(view_election)
 
+#Select variables
 library(tidyverse)
 view_election00 <- select(view_election, country_name, election_date, vote_share, left_right)
 view(view_election00)
+
+#Filter variable country 
+
+view_election01 <- filter(
+  view_election00, 
+  country_name %in% c("Italy", "Germany", "France", "Canada", "Australia", "Japan")) 
+view(view_election01)
+
