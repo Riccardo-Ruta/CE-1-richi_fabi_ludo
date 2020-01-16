a6e6115da5a1ff6bc5a9061a2ede2d1fd77dd105
Anna Ludovica Vinelli <annaludovica.vinelli@studenti.unimi.it>
2020-01-16 17:02:47 +0100

    trying to select

--- script/import_data.R
+++ script/import_data.R
@@ -1,3 +1,6 @@
 library(readr)
 view_election <- read_csv("data/original_data/view_election.csv")
 View(view_election)
+
+library(tidyverse)
+select (view_election, country_name, election_date, vote_share, left_right)
