fa1b7bac717bebd52b590f9b1253f655bacc1e79
FABIANA GEMMA GIANNUZZI <giannuzzifabianagemma@gmail.com>
2020-01-17 18:56:43 +0100

    new graphs for years

--- script/final_script.R
+++ script/final_script.R
@@ -68,7 +68,55 @@ election04 %>%
   group_by(left_right, country_name) %>%
   ggplot(., aes(y=vote_share, x=left_right, group=factor(country_name))) +
   geom_point(aes(col=factor(country_name))) +
-  labs(x="Left_right", y="Vote_share", title = "prova") +
+  labs(x="Left_right", y="Vote_share", title = "Correlation between vote share and political position in the 60's") +
   theme_bw() 
 
-#VOTE_SHARE
+#VOTE_SHARExLEFT_RIGHT: 70's 
+election04 %>% 
+  filter(election_date > "1972-05-07", 
+         election_date <= "1979-10-07") %>%
+  group_by(left_right, country_name) %>%
+  ggplot(., aes(y=vote_share, x=left_right, group=factor(country_name))) +
+  geom_point(aes(col=factor(country_name))) +
+  labs(x="Left_right", y="Vote_share", title = "Correlation between vote share and political position in the 70's") +
+  theme_bw() 
+
+#VOTE_SHARExLEFT_RIGHT: 80's
+election04 %>% 
+  filter(election_date > "1980-06-22", 
+         election_date <= "1987-07-11") %>%
+  group_by(left_right, country_name) %>%
+  ggplot(., aes(y=vote_share, x=left_right, group=factor(country_name))) +
+  geom_point(aes(col=factor(country_name))) +
+  labs(x="Left_right", y="Vote_share", title = "Correlation between vote share and political position in the 80's") +
+  theme_bw() 
+
+#VOTE_SHARExLEFT_RIGHT: 90's 
+election04 %>% 
+  filter(election_date > "1990-02-18", 
+         election_date <= "1998-10-03") %>%
+  group_by(left_right, country_name) %>%
+  ggplot(., aes(y=vote_share, x=left_right, group=factor(country_name))) +
+  geom_point(aes(col=factor(country_name))) +
+  labs(x="Left_right", y="Vote_share", title = "Correlation between vote share and political position in the 90's") +
+  theme_bw() 
+
+#VOTE_SHARExLEFT_RIGHT: 2000's
+election04 %>% 
+  filter(election_date > "2000-06-25", 
+         election_date <= "2009-09-27") %>%
+  group_by(left_right, country_name) %>%
+  ggplot(., aes(y=vote_share, x=left_right, group=factor(country_name))) +
+  geom_point(aes(col=factor(country_name))) +
+  labs(x="Left_right", y="Vote_share", title = "Correlation between vote share and political position in the 2000's") +
+  theme_bw() 
+
+#VOTE_SHARExLEFT_RIGHT: 2010's
+election04 %>% 
+  filter(election_date > "2010-08-21", 
+         election_date <= "2018-03-04") %>%
+  group_by(left_right, country_name) %>%
+  ggplot(., aes(y=vote_share, x=left_right, group=factor(country_name))) +
+  geom_point(aes(col=factor(country_name))) +
+  labs(x="Left_right", y="Vote_share", title = "Correlation between vote share and political position in the 2010's") +
+  theme_bw() 
