e9f567331bb76a386e8aafed2f957147704263f9
FABIANA GEMMA GIANNUZZI <giannuzzifabianagemma@gmail.com>
2020-01-17 11:14:09 +0100

    Modificato nomi dei dataset

--- script/import_data.R
+++ script/import_data.R
@@ -5,36 +5,33 @@ View(view_election)
 
 #Select variables
 library(tidyverse)
-view_election00 <- select(view_election, country_name, election_date, vote_share, left_right)
-view(view_election00)
+election00 <- select(view_election, country_name, election_date, vote_share, left_right)
+view(election00)
 
 #Filter variable country 
 
-view_election01 <- filter(
-  view_election00, 
+election01 <- filter(
+  election00, 
   country_name %in% c("Italy", "Germany", "France", "Canada", "Australia", "Japan")) 
-view(view_election01)
+view(election01)
 
 
 #Recode variable political position
-view_election02 <- mutate(
-  view_election01, 
-  political_pos = ifelse(view_election01$left_right <= 3.3333, "left",
-                   ifelse(view_election01$left_right <= 6.6666, "center",
+election02 <- mutate(
+  election01, 
+  political_pos = ifelse(election01$left_right <= 3.3333, "left",
+                   ifelse(election01$left_right <= 6.6666, "center",
                           "right"))
 )
-view(view_election02)
+view(election02)
 
 #drop missing values 
-view_election03 <- na.omit(view_election02)
-dim(view_election03)
+election03 <- na.omit(election02)
+dim(election03)
 
-apply(view_election03, 2, function(x) sum(is.na(x)))
+apply(election03, 2, function(x) sum(is.na(x)))
 
-view(view_election03)
-
-
-view_election3 <- mutate(political_pos = factor(political_pos, levels = c ("Left", "Center", "Right")))
+view(election03)
 
 
 # Prova grafico Italia da sistemare
@@ -49,9 +46,6 @@ election03 %>%
   xlab("year") +
   theme_bw() +
   theme(legend.position = "bottom")
-
-
-
 
 
 
